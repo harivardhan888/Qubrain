9dc9eb6ca604bf0a15a49bbd76cf15d5c4f50e15
HARI VARDHAN REDDY KUMMETHA <harivardhan888@gmail.com>
2025-09-13 11:10:17 +0530

    Redeployed backend link updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import FlashcardApp from './components/FlashCards'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // import Admin from './components/Admin'
-const api = import.meta.env.VITE_API_URL || (import.meta.env.PROD ? "https://qubrain-backend.vercel.app" : "http://localhost:5000");
+const api = import.meta.env.VITE_API_URL || (import.meta.env.PROD ? "https://qubrain-fc69t9dyf-harivardhan888s-projects.vercel.app" : "http://localhost:5000");
 function App() {
 
   return (
